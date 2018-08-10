2417c977d3a9ec75550056c5c9b6b57429178582
Joshua Langer <joshuarllanger@gmail.com>
2018-08-10 08:46:13 -0500

    component AddContact handles input change

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -6,13 +6,21 @@ class AddContact extends Component {
     address: '',
     email: ''
   };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     const { name, address, email } = this.state;
     return (
       <div className="card mb-3">
         <div className="card-header">Add Contact</div>
         <div className="card-body">
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
               <input
@@ -21,6 +29,7 @@ class AddContact extends Component {
                 className="form-control form-control-lg"
                 placeholder="Full name"
                 value={name}
+                onChange={this.handleChange}
               />
             </div>
             <div className="form-group">
@@ -31,6 +40,7 @@ class AddContact extends Component {
                 className="form-control form-control-lg"
                 placeholder="Address"
                 value={address}
+                onChange={this.handleChange}
               />
             </div>
             <div className="form-group">
@@ -41,6 +51,7 @@ class AddContact extends Component {
                 className="form-control form-control-lg"
                 placeholder="Email"
                 value={email}
+                onChange={this.handleChange}
               />
             </div>
             <input
