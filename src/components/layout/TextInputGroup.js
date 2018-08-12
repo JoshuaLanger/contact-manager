27a8f072eef462e115afb2a47e2144376f3ad2bb
Joshua Langer <joshuarllanger@gmail.com>
2018-08-12 07:59:53 -0500

    small change to TextInputGroup

--- src/components/layout/TextInputGroup.js
+++ src/components/layout/TextInputGroup.js
@@ -11,7 +11,7 @@ const TextInputGroup = ({
 }) => {
   return (
     <div className="form-group">
-      <label htmlFor="name">{label}</label>
+      <label htmlFor={name}>{label}</label>
       <input
         type={type}
         name={name}
